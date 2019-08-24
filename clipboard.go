b5ea5d886e2e836859fb7040efa021e1030866a6
Hans Peyrot <hans@eflow.com.mx>
2019-08-24 16:41:10 -0500

    finished local clipboard

--- clipboard.go
+++ clipboard.go
@@ -1,12 +1,103 @@
 // Package clipboard read/write on clipboard
 package clipboard
 
-// ReadFrom read string from clipboard
-func ReadAll(location string) (string, error) {
-	return readFrom(location)
+import (
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+type Clipboard struct {
+	localPath string
+	url       string
+	key       string
+	secret    string
 }
 
-// WriteTo write string to clipboard
-func WriteAll(text, location string) error {
-	return writeAll(text, location)
+func New() *Clipboard {
+	c := new(Clipboard)
+	return c
+}
+
+func (c *Clipboard) SetLocalPath(path string) {
+	c.localPath = path
+}
+
+func (c *Clipboard) SetCloudPath(url, key, secret string) {
+	c.url = url
+	c.key = key
+	c.secret = secret
+}
+
+func (c *Clipboard) ReadFrom(location string) string {
+	if location == "local" {
+		return readFile(c.localPath)
+	}
+	return ""
+}
+
+func (c *Clipboard) WriteTo(text *string, location string) {
+	if location == "local" {
+		writeFile(c.localPath, text)
+	}
+}
+
+func (c *Clipboard) AppendTo(text *string, location string) {
+	if location == "local" {
+		appendFile(c.localPath, text)
+	}
+}
+
+func (c *Clipboard) writeTo(text *string, location string) {
+}
+
+func (c *Clipboard) appendTo(text *string, location string) {
+
+}
+
+func readFile(path string) string {
+	var clip string
+
+	file, err := os.Open(path)
+	if err != nil {
+		return ""
+	}
+	defer file.Close()
+	//	scanner := bufio.NewScanner(file)
+	//	for scanner.Scan() {
+	//		if clip == "" {
+	//			clip = scanner.Text()
+	//		} else {
+	//			clip = clip + "\n" + scanner.Text()
+	//		}
+	//	}
+	b, err := ioutil.ReadAll(file)
+	clip = string(b)
+	return clip
+}
+
+func writeFile(path string, text *string) {
+	file, err := os.Create(path)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	_, err = io.WriteString(file, *text)
+	if err != nil {
+		return
+	}
+	file.Sync()
+}
+
+func appendFile(path string, text *string) {
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0640)
+	if err != nil {
+		return
+	}
+	_, err = io.WriteString(file, *text)
+	if err != nil {
+		return
+	}
+	file.Sync()
 }
