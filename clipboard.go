ead5d5a1d66396810b1f7fa6a600ab010bf6403a
Hans Peyrot <hans@eflow.com.mx>
2019-09-25 17:23:37 -0500

    added methods to set and reset cloud account

--- clipboard.go
+++ clipboard.go
@@ -2,15 +2,27 @@
 package clipboard
 
 import (
+	"bytes"
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/gtank/cryptopasta"
 )
 
 type Clipboard struct {
 	localPath string
 	url       string
-	key       string
+	apikey    string
+	apipass   string
 	secret    string
 	disabled  bool
 }
@@ -24,30 +36,48 @@ func (c *Clipboard) SetLocalPath(path string) {
 	c.localPath = path
 }
 
-func (c *Clipboard) SetCloudPath(url, key, pass, secret string) {
-	if key == "" || pass == "" {
-		c.disabled = true
+func (c *Clipboard) SetCloudPath(url, apikey, apipass, secret string) error {
+	c.disabled = true
+	if url == "" || apikey == "" || apipass == "" || secret == "" {
+		return errors.New("incomplete cloud connection information")
+	} else if strings.Contains(url, "https") == false && strings.Contains(url, "localhost") == false {
+		return errors.New("incorrect url")
 	}
+	c.disabled = false
 	c.url = url
-	c.key = key
+	c.apikey = apikey
+	c.apipass = apipass
 	c.secret = secret
+	return nil
 }
 
-func (c *Clipboard) ReadFrom(location string) string {
+func (c *Clipboard) SetUpCloudService() string {
+	emtpy := ""
+	return c.writeCloud("setup", &emtpy)
+}
+
+func (c *Clipboard) ResetCloudService() string {
+	emtpy := ""
+	return c.writeCloud("reset", &emtpy)
+}
+
+func (c *Clipboard) ReadFrom(location, cmd string) string {
 	if location == "local" {
 		return readFile(c.localPath)
 	} else if location == "cloud" {
-		return c.readCloud()
+		return c.readCloud(cmd)
 	}
 	return ""
 }
 
-func (c *Clipboard) WriteTo(text *string, location string) string {
+func (c *Clipboard) WriteTo(text *string, location, cmd string) string {
 	msg := ""
 	if location == "local" {
 		writeFile(c.localPath, text)
 	} else if location == "cloud" {
-		msg = c.writeCloud(text)
+		msg = c.writeCloud(cmd, text)
+	} else {
+		return ""
 	}
 	return msg
 }
@@ -105,37 +135,118 @@ func appendFile(path string, text *string) {
 // Cloud clipboard
 // -----------------------------------
 
-// Request and Response are encripted with user password
-// Document is encript with user passphrase
-
-// Cmd		: saveclip, getclip, uploadfile, downloadfile, uploadsettings, downloadsetting, uploadmicroide, downloadmicroide
-// Document	: BASE64 encoded clip, file, settings or microide
+// Cmd		: clip, file, setting, microide, setup
+// Document	: Encripted, BASE64 clip, file, settings or microide
 
 type request struct {
 	Cmd      string `json:"cmd"`
+	Key      string `json:"apikey"`
+	Pass     string `json:"apipass"`
 	Document string `json:"document,omitempty"`
 }
 
-// Status	: true=success, false==error
+// Success	: true, false
 // ErrMsg	: Error message
 // Document	: BASE64 encoded clip, file, settings or microide
 
 type response struct {
-	Status   bool   `json:"status"`
+	Success  bool   `json:"succcess"`
 	ErrMsg   string `json:"errmsg"`
 	Document string `json:"document"`
 }
 
-func (c *Clipboard) readCloud() string {
+func (c *Clipboard) readCloud(cmd string) string {
 	if c.disabled {
 		return ""
+	}
+	// Create requesto and send to server
+	var jresp response
+	req := new(request)
+	req.Cmd = cmd
+	req.Key = c.apikey
+	req.Pass = c.apipass
+	jreq, _ := json.Marshal(req)
+	hreq, err := http.NewRequest("POST", c.url+"/get", bytes.NewBuffer(jreq))
+	hreq.Header.Set("Content-Type", "application/json")
+	client := &http.Client{
+		Timeout: time.Second * 20,
+	}
+	resp, err := client.Do(hreq)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+	sresp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+	if err = json.Unmarshal(sresp, &jresp); err != nil {
+		return ""
+	}
+	if jresp.Success == false {
+		return jresp.ErrMsg
+	}
+	text := DecriptData(c.secret, jresp.Document)
+	return string(text)
+}
+
+func (c *Clipboard) writeCloud(cmd string, text *string) string {
+	if c.disabled {
+		return "Service not available"
+	}
+	var jresp response
+	req := new(request)
+	req.Cmd = "save" + cmd
+	req.Key = c.apikey
+	req.Pass = c.apipass
+	if *text != "" {
+		req.Document = EncriptData(c.secret, *text)
+	}
+	jreq, _ := json.Marshal(req)
+	return string(jreq)
+	hreq, err := http.NewRequest("POST", c.url+"/put", bytes.NewBuffer(jreq))
+	hreq.Header.Set("Content-Type", "application/json")
+	client := &http.Client{
+		Timeout: time.Second * 20,
+	}
+	resp, err := client.Do(hreq)
+	if err != nil {
+		return err.Error()
+	}
+	defer resp.Body.Close()
+	sresp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err.Error()
+	}
+	if err = json.Unmarshal(sresp, &jresp); err != nil {
+		return err.Error()
+	}
+	if jresp.Success == false {
+		return jresp.ErrMsg
 	}
 	return ""
 }
 
-func (c *Clipboard) writeCloud(text *string) string {
-	if c.disabled {
-		return "Service not available"
+// Crypt routines
+
+func getKeyHash(key string) *[32]byte {
+	b := [32]byte{}
+	h := md5.New()
+	h.Write([]byte(key))
+	str := hex.EncodeToString(h.Sum(nil))
+	for k, v := range []byte(str) {
+		b[k] = byte(v)
 	}
-	return ""
+	return &b
+}
+
+func EncriptData(key, data string) string {
+	cdata, _ := cryptopasta.Encrypt([]byte(data), getKeyHash(key))
+	return base64.StdEncoding.EncodeToString(cdata)
+}
+
+func DecriptData(key, ecdata string) string {
+	cdata, _ := base64.StdEncoding.DecodeString(ecdata)
+	data, _ := cryptopasta.Decrypt(cdata, getKeyHash(key))
+	return string(data)
 }
